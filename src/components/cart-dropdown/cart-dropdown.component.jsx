0c61cdaacd52a282090e2cf8a3973ac455f7a264
piyush9199 <piyush9199@gmail.com>
2024-12-25 14:51:33 +0530

    cart empty logic, directory page routing

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -18,8 +18,13 @@ export function CartDropdown() {
         <div className="cart-dropdown-container">
             <div className="cart-items">
             {
-                cartItems.map((item)=>
+                cartItems.length? 
+                (cartItems.map((item)=>
                     (<CartItem key={item.id} cartItem={item} />)
+                ))
+                :
+                (
+                    <span className="empty-cart-message">Your cart is empty</span>
                 )
             }
             </div>
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,12 +1,17 @@
+import { useNavigate } from 'react-router-dom'
 import './directory-item.styles.scss'
 
 
 export function DirectoryItem({ category }) {               //{category} instead of category= destructuring
+    const navigate = useNavigate()
+    function navigateHandler(){
+        navigate(`/shop/${category.title.toLowerCase()}`)
+    }
     return (
-        <div className="directory-item-container">
+        <div className="directory-item-container" onClick={navigateHandler} >
             <div className="background-image" style={{ backgroundImage: `url(${category.imageUrl})` }} ></div>
             <div className="body">
-                <h2>{category.title}</h2>
+                <h2>{category.title.toUpperCase()}</h2>
                 <p>Shop Now</p>
             </div>
         </div>
